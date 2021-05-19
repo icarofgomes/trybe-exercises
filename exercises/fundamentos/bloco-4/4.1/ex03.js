a182c212fd35d6730e65cfbdd52b752e5f0f0dcf
Icaro Ferreira <icaro.iky@gmail.com>
2021-05-19 15:59:46 -0300

    Ex. de 1 a 5

---
+++ exercises/fundamentos/bloco-4/4.1/ex03.js
@@ -0,0 +1,13 @@
+let a = 9;
+let b = 11;
+let c = 7;
+
+if (a > b && a > c){
+  console.log(a);
+}
+else if (b > a && b > c){
+  console.log(b)
+}
+else{
+  console.log(c);
+}
---
+++ exercises/fundamentos/bloco-4/4.1/ex04.js
@@ -0,0 +1,11 @@
+let variavel = 0;
+
+if (variavel > 0){
+  console.log('positive');
+}
+else if (variavel < 0){
+  console.log('negative');
+}
+else{
+  console.log('zero')
+}
