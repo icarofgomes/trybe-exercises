ab3d116d2aa5678f43e17aeeaa70a8a37a5808f7
Icaro Ferreira <icaro.iky@gmail.com>
2021-05-24 15:27:21 -0300

    Ex. 5 Ajuste

--- exercises/fundamentos/bloco-4/4.4/ex05.js
+++ exercises/fundamentos/bloco-4/4.4/ex05.js
@@ -2,26 +2,20 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
 };
 
 let info2 = {
   personagem: 'Tio Patinhas',
   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas'
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
 };
-
 
 for (let i in info){
-  console.log(info[i] + ' e ' + info2[i]);
-};
-
-info['recorrente'] = 'Sim';
-info2['recorrente'] = 'Sim';
-
-if (info['recorrente'] === 'Sim' && info2['recorrente'] === 'Sim'){
-  console.log('Ambos recorrentes');
-}else if ( info['recorrente'] === 'Não' && info2['recorrente'] === 'Não'){
-  console.log('Ambos não são recorrentes');
-}else{
-  console.log('Apenas um dos dois é recorrente!');
+  if ( info[i] === info.recorrente && info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+    console.log('Ambos recorrentes');
+  } else{
+    console.log(info[i] + ' e ' + info2[i]);
+  };
 };
