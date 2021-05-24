62a70f8aa11522e5b5bae645e55e30d05779dc97
Icaro Ferreira <icaro.iky@gmail.com>
2021-05-24 18:47:35 -0300

    parte2 ex 3

--- exercises/fundamentos/bloco-4/4.4/parte2/ex02.js
+++ exercises/fundamentos/bloco-4/4.4/parte2/ex02.js
@@ -4,10 +4,10 @@ function maiorValor(array){
   for (let i = 0; i < array.length; i += 1){
     if (array[i] >= maior){
       maior = teste[i];
-    }
+    };
   };
   return array.indexOf(maior);
-}
+};
 
 let teste = [2, 3, 6, 7, 10, 1];
 console.log(maiorValor(teste));
