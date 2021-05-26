aa3c3d0e40890ea2ca7264dff7d86cc4779695ca
Icaro Ferreira <icaro.iky@gmail.com>
2021-05-26 19:37:59 -0300

    Atualização ex0 bloco5

--- exercises/fundamentos/bloco-5/5.1/ex00/script.js
+++ exercises/fundamentos/bloco-5/5.1/ex00/script.js
@@ -1,11 +1,12 @@
 document.querySelector('#header-container').style.background = 'MediumSeaGreen';
 document.querySelector(".emergency-tasks").style.background = "DarkSalmon";
-let repeat = 2;
-for (let i = 0; i < repeat; i += 1){
-  document.getElementsByTagName("h3")[i].style.background = "DeepPink";
+document.querySelector(".no-emergency-tasks").style.background = "DarkSalmon";
+let element1 = document.querySelectorAll(".emergency-tasks h3");
+for (let i = 0; i < element1.length; i += 1){
+  element1[i].style.background = "DeepPink";
 }
-document.querySelector('.no-emergency-tasks').style.background = "Gold";
-for (let i = 3; i > repeat-1; i -= 1){
-  document.getElementsByTagName("h3")[i].style.background = "Black";
+let element2 = document.querySelectorAll(".no-emergency-tasks h3");
+for (let i = 0; i < element2.length; i += 1){
+  element2[i].style.background = "Black";
 }
 document.querySelector('#footer-container').style.background = 'DarkSlateGrey';
