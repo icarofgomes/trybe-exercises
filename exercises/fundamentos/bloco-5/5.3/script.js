b9b3abeadc725cf95ad3e819bf1a68c51a11d785
Icaro Ferreira <icaro.iky@gmail.com>
2021-05-29 19:54:40 -0300

    Ex. 2

--- exercises/fundamentos/bloco-5/5.3/script.js
+++ exercises/fundamentos/bloco-5/5.3/script.js
@@ -14,6 +14,8 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// Ex. 1
 let ulDays = document.querySelector('#days');
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -36,3 +38,13 @@ function setDays (){
   }
 }
 setDays();
+
+// Ex. 2
+
+function addBtn(str){
+  let btn = document.createElement('button');
+  let divBtn = document.querySelector('.buttons-container');
+  btn.innerHTML = str;
+  divBtn.appendChild(btn);
+}
+addBtn('Feriados');
