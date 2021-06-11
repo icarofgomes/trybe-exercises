b6bb7e99b7a63b82f7af12a84c9da45c3395ae3c
Icaro Ferreira <icaro.iky@gmail.com>
2021-06-11 19:18:16 -0300

    Impressão de erro na tela, retorno de campo com todas as respostas, adição de botão de reset.

--- exercises/fundamentos/bloco-6/6.1/ex01/script.js
+++ exercises/fundamentos/bloco-6/6.1/ex01/script.js
@@ -10,6 +10,7 @@ const inputAbstract = document.querySelector('#last-job');
 const inputPosition = document.querySelector('#input-position');
 const inputDescription = document.querySelector('#input-description');
 const resultField = document.querySelector('#result-forms');
+const resetButton = document.querySelector('#reset-button');
 const states = 
 [
   'Acre - AC',
@@ -44,21 +45,21 @@ const states =
 function stateFieldLoad(){
   for (let i = 0; i < states.length; i += 1){
     let newOptionState = document.createElement('option');
-    let getInitials = states[i].slice(states[i].indexOf('-')+2).toLowerCase();
+    let getInitials = states[i].slice(states[i].indexOf('-')+2);
     newOptionState.value = getInitials;
     newOptionState.innerHTML = states[i];
     selectStateField.appendChild(newOptionState);
   }
 }
-window.addEventListener('load', stateFieldLoad);
 
 function verifyName(){
   let trimmedName = inputName.value.trim();
   if (trimmedName.length === 0 || trimmedName.length > 40){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Nome inválido: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  } else {
+    return false;
   }
 }
 
@@ -66,9 +67,10 @@ function verifyEmail(){
   let trimmedEmail = inputEmail.value.trim();
   if (trimmedEmail.length === 0 || trimmedEmail.length > 50){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Email inválido: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
   }
 }
 
@@ -76,9 +78,10 @@ function verifyCPF(){
   let trimmedCPF = inputCPF.value.trim();
   if (trimmedCPF.length === 0 || trimmedCPF.length > 11){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `CPF inválido: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
   }
 }
 
@@ -86,9 +89,10 @@ function verifyAdress(){
   let trimmedAdress = inputAdress.value.trim();
   if (trimmedAdress.length === 0 || trimmedAdress.length > 200){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Endereço inválido: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
   }
 }
 
@@ -96,9 +100,10 @@ function verifyCity(){
   let trimmedCity = inputCity.value.trim();
   if (trimmedCity.length === 0 || trimmedCity.length > 28){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Cidade inválida: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
   }
 }
 
@@ -106,9 +111,10 @@ function verifyAbstract(){
   let trimmedAbstract = inputAbstract.value.trim();
   if (trimmedAbstract.length === 0 || trimmedAbstract.length > 1000){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Resumo inválido: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
   }
 }
 
@@ -116,9 +122,10 @@ function verifyPosition(){
   let trimmedPosition = inputPosition.value.trim();
   if (trimmedPosition.length === 0 || trimmedPosition.length > 40){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Cargo inválido: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
   }
 }
 
@@ -126,9 +133,10 @@ function verifyDescription(){
   let trimmedDescription = inputDescription.value.trim();
   if (trimmedDescription.length === 0 || trimmedDescription.length > 500){
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Descrição do cargo inválida: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
   }
 }
 
@@ -139,24 +147,48 @@ function verifyDate() {
   const year = itemContent.slice(6, 10);
   if (day < 1 || day > 31 || mounth < 1 || mounth > 12 || year < 0) {
     let errorMsg = document.createElement('p');
-    errorMsg.classList.add('error');
     errorMsg.innerHTML = `Formato de data inválido: Tente novamente!`;
     resultField.appendChild(errorMsg);
+  }else {
+    return false;
+  }
+}
+
+function resetResults(){
+  resultField.innerHTML = '';
+  if (resultField.classList.contains('error')){
+    resultField.classList.remove('error');
+  }
+  if (resultField.classList.contains('sucess')){
+    resultField.classList.remove('sucess');
   }
 }
 
 function validateData(event){
-  resultField.innerHTML = '';
+  resetResults();
   event.preventDefault();
   
-  verifyName();
-  verifyEmail();
-  verifyCPF();
-  verifyAdress();
-  verifyCity();
-  verifyAbstract();
-  verifyPosition();
-  verifyDescription();
-  verifyDate();
+  if (verifyName() !== false || verifyEmail() !== false || verifyCPF() !== false ||
+      verifyAdress() !== false || verifyCity() !== false || verifyAbstract() !== false ||
+      verifyPosition() !== false || verifyDate() !== false){
+        resultField.classList.add('error');
+  } else {
+    resultField.classList.add('sucess');
+    resultField.innerHTML += `<strong>Nome:</strong> ${inputName.value}<br>`;
+    resultField.innerHTML += `<strong>Email:</strong> ${inputEmail.value}<br>`;
+    resultField.innerHTML += `<strong>CPF:</strong> ${inputCPF.value}<br>`;
+    resultField.innerHTML += `<strong>Endereço:</strong> ${inputAdress.value}<br>`;
+    resultField.innerHTML += `<strong>Cidade:</strong> ${inputCity.value}<br>`;
+    resultField.innerHTML += `<strong>Estado:</strong> ${selectStateField.value}<br>`;
+    const inputType = document.querySelector('input[name="type"]:checked');
+    resultField.innerHTML += `<strong>Tipo:</strong> ${inputType.value}<br>`;
+    resultField.innerHTML += `<strong>Resumo:</strong> ${inputAbstract.value}<br>`;
+    resultField.innerHTML += `<strong>Cargo:</strong> ${inputPosition.value}<br>`;
+    resultField.innerHTML += `<strong>Descrição do cargo:</strong> ${inputDescription.value}<br>`;
+    resultField.innerHTML += `<strong>Data de Início:</strong> ${inputedDate.value}`;
+  }
 }
+
+window.addEventListener('load', stateFieldLoad);
 subitButton.addEventListener('click', validateData);
+resetButton.addEventListener('click', resetResults);
