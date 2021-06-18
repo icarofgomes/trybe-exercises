d3ebad2ea54bd98a78a9c074f75689d150300dd6
Icaro Ferreira <icaro.iky@gmail.com>
2021-06-18 19:06:24 -0300

    Finalizada a parte2

--- exercises/fundamentos/bloco-7/7.2/ex01/parte2.js
+++ exercises/fundamentos/bloco-7/7.2/ex01/parte2.js
@@ -33,3 +33,17 @@ const listOfValues = obj => Object.values(obj);
 
 // Ex 5
 const allLessons = {lesson1: lesson1, lesson2: lesson2, lesson3: lesson3};
+
+// Ex 6
+const totalStudents = () => {
+  const lessonStudents1 = allLessons.lesson1.numeroEstudantes;
+  const lessonStudents2 = allLessons.lesson2.numeroEstudantes;
+  const lessonStudents3 = allLessons.lesson3.numeroEstudantes;
+  return lessonStudents1 + lessonStudents2 + lessonStudents3;
+}
+
+// Ex 7
+const getValueByNumber = (obj, number) => Object.values(obj)[number];
+
+// Ex 8
+const verifyPair = (obj, key, value) => obj[key] === value ? true : false;
