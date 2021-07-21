8acfec0831abf474ccbc5a24b5b4e7d421db6292
Icaro Ferreira <icaro.iky@gmail.com>
2021-07-21 12:04:56 -0300

    Aula

---
+++ exercises/front-end/bloco-11/11.2/aula/aula/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <Greeting name="Icaro"/>
+  );
+}
+
+export default App;
