398814ab5d65e1ba043782b9882fd31a72dd8590
Icaro Ferreira <icaro.iky@gmail.com>
2021-07-21 20:05:47 -0300

    Aula até type-props

--- exercises/front-end/bloco-11/11.2/aula/aula/src/App.js
+++ exercises/front-end/bloco-11/11.2/aula/aula/src/App.js
@@ -1,36 +1,13 @@
-import './App.css';
+// arquivo App.js, criado pelo create-react-app, modificado
 import React from 'react';
-import Order from './components/Order';
+import './App.css';
+import UserProfile from './components/UserProfile';
 
 class App extends React.Component {
-  render () {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
-      }
-    };
+  render() {
 
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
 
-    return (
-      <div className="App">
-        <h1>Orders recently created:</h1>
-        <Order order={headphone}/>
-        <Order order={energyDrink}/>
-      </div>
-    );
+    return ();
   }
 }
 
