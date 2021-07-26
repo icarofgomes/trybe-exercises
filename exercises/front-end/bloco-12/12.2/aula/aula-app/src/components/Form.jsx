68d8446eed341e7e93ebeaa3b83594a146770e2b
Icaro Ferreira <icaro.iky@gmail.com>
2021-07-26 19:57:33 -0300

    Fixando o conteúdo parte 3

--- exercises/front-end/bloco-12/12.2/aula/aula-app/src/components/Form.jsx
+++ exercises/front-end/bloco-12/12.2/aula/aula-app/src/components/Form.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import FullName from './FullName';
+import Age from './Age';
 
 class Form extends React.Component {
   constructor() {
     super();
     this.handleState = this.handleState.bind(this);
+    this.setState = this.setState.bind(this);
 
     this.state = {
-      name: '',
-      lastName: '',
+      fullName: '',
       age: '',
       abstract: '',
       selectState: '',
@@ -15,12 +17,30 @@ class Form extends React.Component {
     }
   }
 
+  setError() {
+    const {fullName, age, abstract, selectState, terms} = this.state;
+
+    const errorCases = [
+      !fullName.length,
+      !age.length,
+      !abstract.length,
+      !selectState.length,
+      !terms,
+    ];
+
+    const checkForm = errorCases.every((error) => error !== true);
+
+    this.setState({
+      formWithErrors: !checkForm,
+    })
+  }
+
   handleState({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    })
+    }, () => {this.setError();}) 
   }
 
   render() {
@@ -29,37 +49,18 @@ class Form extends React.Component {
         <h1>Aula Form</h1>
         <fieldset>
           <legend>Dados Pessoais</legend>
-          <label htmlFor="name">Nome:
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={this.state.name}
-              onChange={this.handleState}
-            />
-          </label>
-          <label htmlFor="lastName">Sobrenome:
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              value={this.state.lastName}
-              onChange={this.handleState}
-            />
-          </label>
-          <label htmlFor="age">Idade:
-            <input
-              type="number"
-              name="age"
-              id="age"
-              value={this.state.age}
-              onChange={this.handleState}
-            />
-          </label>
+          <FullName
+            value={this.state.fullName}
+            handleState={this.handleState}
+          />
+          <Age
+            value={this.state.age}
+            handleState={this.handleState}
+          />
         </fieldset>
         <fieldset>
           <legend>Sobre você</legend>
-          <label htmlFor="abstract">Resumo:
+          <label htmlFor="abstract">Resumo
             <textarea
               name="abstract"
               id="abstract"
@@ -72,7 +73,7 @@ class Form extends React.Component {
         </fieldset>
         <fieldset>
           <legend>Endereço</legend>
-          <label htmlFor="selectState">Selecione o seu Estado:
+          <label htmlFor="selectState">Selecione o seu Estado
             <select
               name="selectState"
               id="selectState"
@@ -85,7 +86,7 @@ class Form extends React.Component {
             </select>
           </label>
         </fieldset>
-        <label htmlFor="terms">Concordo com os termos de uso:
+        <label className="terms" htmlFor="terms">Concordo com os termos de uso
             <input
               type="checkbox"
               name="terms"
